48fdac83c06e6da793617e3cb3fcea9a50ea16ba
AnjanaTharanga24 <anjana24myprof@gmail.com>
2024-06-06 13:10:16 +0000

    new update

--- backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.database_mapping;
+package com.example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DatabaseMappingApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
